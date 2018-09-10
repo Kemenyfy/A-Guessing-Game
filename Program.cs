797416dbb9508e50ff9909673120a1e482d1be83
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-09-10 17:36:45 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simple_console_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
