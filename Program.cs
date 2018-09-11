1dcf363f2d67852fdbf980874d0bfcf805cd0c2b
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-09-10 20:53:24 -0400

    Added some Game Logic

--- Program.cs
+++ Program.cs
@@ -1,38 +1,38 @@
 ﻿using System;
 
-namespace dotnet_test
+namespace GuessingGame
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Think of a number between 1 and 100.");
+            var min = 1;
+            var max = 100;
+            var Yes = "Yes";
+            var Attempts = 0;
+
+            Console.WriteLine($"Think of a number between {min} and {max}.");
             Console.WriteLine("I will be able to guess in 10 tries or less");
             Console.WriteLine("Ok? (Yes or No)");
             var answer = Console.ReadLine();
             if (answer == "No")
             {
-                Console.WriteLine("I really wanted to play");
+                Console.WriteLine("I really wanted to play with you... Good bye.");
             } 
             else
             {
                 Console.WriteLine("Ok, let's Play...");
             }
             Console.WriteLine("Is your number 50? (Yes - H for Higher - L for Lower)");
-            var answer50 = Console.ReadLine();
-            if (answer50 == "Yes")
+            var userAnswer = Console.ReadLine();
+            if (userAnswer.Equals($"{Yes}"));
             {
                 Console.WriteLine("It took me 1 try!");
             }
-            else if (answer50 == "H")
+            if (Attempts >= 10)
             {
-                Console.WriteLine("Is your number 75? (Yes - H for Higher - L for Lower)");
-            }
-            else
-            {
-                Console.WriteLine("Is your number 25? (Yes - H for Higher - L for Lower)");
+                Console.WriteLine("I've tried enough, I give up!");
             }
         }
     }
 }
-
